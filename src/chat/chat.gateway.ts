6fd059b0c2f81ea70394d33336f35dcf4070836f
Caio loyer <ricartecaio338@gmail.com>
2025-12-11 00:26:28 -0300

    chore: init

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,33 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: { origin: '*' },
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente desconectado: ${client.id}`);
+  }
+
+  @SubscribeMessage('send_message')
+  handleMessage(@MessageBody() payload: { user: string; message: string }) {
+    this.server.emit('new_message', {
+      ...payload,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
